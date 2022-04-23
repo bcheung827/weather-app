d6e0657333145c7d57c16648f633ccf731193208
bcheung827 <83896907+bcheung827@users.noreply.github.com>
2022-04-23 09:07:59 -0400

    js card display

--- assets/script.js
+++ assets/script.js
@@ -35,41 +35,60 @@ function weatherCondition(lat, lon) {
         pUV.text(`UV Index: ${uvi}`);
         $('.currentWeather').append(pTemp, pHumidity, pWind_speed, pUV)
 
-        var iconCode = cityWeatherResponse.weather[0].icon;
-        iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+       // var iconCode = cityWeatherResponse.weather[0].icon;
+       // iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+    futureCondition(cityWeatherResponse)
     })
 }
-function futureCondition(lat, lon) {
-    var futureURL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+function futureCondition(futureResponse) {
 
-    $.ajax({
-        url: futureURL,
-        method: "GET"
-    }).then(function (futureResponse) {
         console.log(futureResponse);
         $('.fiveDay').empty();
         //create a var that holds UV index value
         for (var i = 0; i < 5; i++) {
 
-            var temp = futureResponse.list[i].main.temp;
-            var humidity = futureResponse.list[i].main.humidity;
-            var wind_speed = futureResponse.list[i].wind.speed;
+            var currDateConvert = new Date(futureResponse.daily[i].dt * 1000).toISOString();
+            var currDate = currDateConvert.split("T")[0] //["Fri", "Apr", "22", "2022", ""]
+           // console.log(JSON.stringify(currDateConvert))
+            var temp = futureResponse.daily[i].temp.day;
+            var humidity = futureResponse.daily[i].humidity;
+            var wind_speed = futureResponse.daily[i].wind_speed;
 
-            //Create a p element container to display UV Index value
-            var pTemp = $('<p/>');
-            var pHumidity = $('<p/>');
-            var pWind_speed = $('<p/>');
-            var iconURL = $('<img/>');
-            //Display value on container
-            pTemp.text(`Temperature: ${temp} `);
-            pHumidity.text(`Humidity: ${humidity}`);
-            pWind_speed.text(`Wind Speed: ${wind_speed}`);
-            $('.fiveDay').append(pTemp, pHumidity, pWind_speed)
+            // var pcurrDate = $('<p/>');
+            // var pTemp = $('<p/>');
+            // var pHumidity = $('<p/>');
+            // var pWind_speed = $('<p/>');
+            // var iconURL = $('<img/>');
+            // //Display value on container
+            // pcurrDate.text(`Date: ${currDate}`)
+            // pTemp.text(`Temperature: ${temp} `);
+            // pHumidity.text(`Humidity: ${humidity}`);
+            // pWind_speed.text(`Wind Speed: ${wind_speed}`);
+            // $('.fiveDay').append(pcurrDate, pTemp, pHumidity, pWind_speed)
+            var futureCard = $(`
+            <div class="card col-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <h6 class="card-subtitle mb-2 text-muted">Date: ${currDate}</h6>
+        <img src="https://openweathermap.org/img/w/" alt="" ></img>
+        <p class="card-text">Temp: ${temp}°F</p>
+        <p class="card-text">Humidity: ${humidity}</p>
+        <p class="card-text">Wind: ${wind_speed}</p>
+      </div>
+    </div>
+        `);
+    
+            $(".fiveDay").append(futureCard);
+        
         }
-        var iconCode = futureResponse.list.weather[0].icon;
-        iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
-    })
-}
+        //var currDate = moment.unix(currDate).format("MM/DD/YYYY");
+        //var iconCode = futureResponse.list.weather[0].icon;
+        //iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+
+    }
+
+
 //retrieve latitude and longitude of coordinates for the inputted city
 function getLatLon(city) {
     var queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey
@@ -81,8 +100,9 @@ function getLatLon(city) {
         console.log(cityWeatherResponse);
         var lat = cityWeatherResponse[0].lat;
         var lon = cityWeatherResponse[0].lon;
+        console.log(lat,lon)
         weatherCondition(lat, lon)
-        futureCondition(lat, lon)
+        //futureCondition(lat, lon)
         $(".currentWeather").css("display", "block");
         $("cityDetail").empty();
 
@@ -113,13 +133,13 @@ $("#search").on("click", function (event) {
     console.log(searchHistoryList);
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
 
     if (searchHistoryArr !== null) {
         var lastSearchedIndex = searchHistoryArr.length - 1;
         var lastSearchedCity = searchHistoryArr[lastSearchedIndex];
-        weatherCondition(lastSearchedCity);
+       // weatherCondition(lastSearchedCity);
         console.log(`Last searched city: ${lastSearchedCity}`);
     }
 });
